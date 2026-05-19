f9645f80ebb5cd327f95d75c29e606eb15f2f77a
Petter Stensig <petter.stensig@gmail.com>
2026-05-19 14:33:18 +0200

    Add: favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -20,6 +20,8 @@ export default function Layout({ children, title, description, canonical }) {
         <meta property="og:title" content={siteTitle} />
         <meta property="og:description" content={metaDesc} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KH5WL9F2');`}} />
       </Head>
